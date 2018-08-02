2a5fc7a4ccb784cf126920ffd138d8596ebbf775
Borja Caballero <borja.caba@gmail.com>
2018-08-02 12:36:19 +0200

    fix logout link

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res, next) =>{
+    if (req.session.currentUser) {
+        next();
+    } else {
+        res.redirect("/auth/login");
+    }
+}, (req, res, next) => {
     res.render('profile')
 })
  
